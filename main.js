aa828a1798fca3d4b79b47ca6327594573c71071
konglongchen <a8960622446@163.com>
2025-08-07 09:18:55 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggleSidebar').onclick = () => {
-  const sidebar = document.getElementById('sidebar');
-  sidebar.classList.toggle('collapsed');
-};
-
-function loadModule(module) {
-  window.location.href = `modules/${module}/index.html`;
-}
