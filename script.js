1b592e885d6859be3abad26b9bc68bc953c16cda
konglongchen <a8960622446@163.com>
2025-08-07 08:24:49 +0800

    smart-xuezhi v1.5
    •新增移动端版本
    •优化UI
    •优化文稿输入
    •新增PDF导出功能

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 
-function downloadFile(filename) {
-    const content = document.getElementById('content').value;
-    const blob = new Blob([content], { type: 'text/plain' });
-    const a = document.createElement("a");
-    a.href = URL.createObjectURL(blob);
-    a.download = filename;
-    a.click();
+const pageKey = location.pathname;
+
+window.onload = function () {
+    const savedTitle = localStorage.getItem(pageKey + "_title");
+    const savedNumber = localStorage.getItem(pageKey + "_number");
+    const savedBody = localStorage.getItem(pageKey + "_body");
+    if (savedTitle) document.getElementById("doc-title").innerText = savedTitle;
+    if (savedNumber) document.getElementById("doc-number").innerText = savedNumber;
+    if (savedBody) document.getElementById("doc-body").innerHTML = savedBody;
+};
+
+function autoSave() {
+    localStorage.setItem(pageKey + "_title", document.getElementById("doc-title").innerText);
+    localStorage.setItem(pageKey + "_number", document.getElementById("doc-number").innerText);
+    localStorage.setItem(pageKey + "_body", document.getElementById("doc-body").innerHTML);
+}
+
+function downloadPDF() {
+    const title = document.getElementById("doc-title").innerText;
+    const number = document.getElementById("doc-number").innerText;
+    const body = document.getElementById("doc-body").innerText;
+
+    const win = window.open('', '', 'height=800,width=800');
+    win.document.write('<html><head><title>' + title + '</title></head><body>');
+    win.document.write('<h1 style="color:red;text-align:left;font-family:SimSun;">' + title + '</h1>');
+    win.document.write('<div style="font-family:SimSun;font-size:14px;">' + number + '</div>');
+    win.document.write('<div style="font-family:SimSun;font-size:16px;line-height:1.8;">' + body.replace(/\n/g, "<br>") + '</div>');
+    win.document.write('</body></html>');
+    win.document.close();
+    win.print();
 }
